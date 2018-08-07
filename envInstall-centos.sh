377337d2dee7514fa1ac28d6de6febd640a6f151
shiningbeer <shiningbeer@163.com>
2018-08-07 09:38:45 +0800

    s

---
+++ envInstall-centos.sh
@@ -0,0 +1,11 @@
+sudo yum install -y npm
+sudo npm install -g n
+sudo n stable
+sudo yum install -y mongodb
+sudo yum install -y mongodb-server
+sudo yum install -y screen
+sudo yum install -y python-pip
+sudo pip install setuptools
+sudo pip install pymongo
+sudo pip install IPy
+sudo yum install -y zmap
---
+++ envInstall-ubuntu.sh
@@ -0,0 +1,11 @@
+sudo apt install -y npm
+sudo npm install -g n
+sudo n stable
+sudo apt install -y mongodb
+service mongodb start
+sudo apt install -y screen
+sudo apt install -y python-pip
+sudo pip install setuptools
+sudo pip install pymongo
+sudo pip install IPy
+sudo apt install -y zmap
